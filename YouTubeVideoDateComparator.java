360fd8b2437a18e35ad9d0f657d738f3abe5bbf0
Harry Le <harry.le2420@gmail.com>
2024-11-13 08:54:15 +1030

    Delete YouTubeVideoDateComparator.java

--- YouTubeVideoDateComparator.java
+++
@@ -1,8 +0,0 @@
-import java.util.Comparator;
-
-public class YouTubeVideoDateComparator implements Comparator<YouTubeVideo> {
-    @Override
-    public int compare(YouTubeVideo o1, YouTubeVideo o2) {
-        return o1.getDate().compareTo(o2.getDate());
-    }
-}
