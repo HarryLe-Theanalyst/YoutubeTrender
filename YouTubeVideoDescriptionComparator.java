5488d6d803f04bb0280659c5c84e0673515584e4
Harry Le <harry.le2420@gmail.com>
2024-11-13 08:54:21 +1030

    Delete YouTubeVideoDescriptionComparator.java

--- YouTubeVideoDescriptionComparator.java
+++
@@ -1,8 +0,0 @@
-import java.util.Comparator;
-
-public class YouTubeVideoDescriptionComparator implements Comparator<YouTubeVideo> {
-    @Override
-    public int compare(YouTubeVideo o1, YouTubeVideo o2) {
-        return Integer.compare(o1.getDescription().length(), o2.getDescription().length());
-    }
-}
